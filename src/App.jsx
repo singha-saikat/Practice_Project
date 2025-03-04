c2de4c2964473a470726600506cb17fc40e31e80
Saikat <sachinsingha7899@gmail.com>
2025-03-04 18:18:36 +0600

    Do practice with 3.11:React

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import Counter from "./components/Counter";
+import { useState } from 'react';
+import Chat from './components/Chat.jsx';
+import ContactList from './components/Chat.jsx';
 
-const App = () => {
-  const counter = <Counter />;
+export default function App() {
+  const [to, setTo] = useState(contacts[0]);
   return (
     <div>
-      {counter}
-      {counter}
+      <ContactList
+        contacts={contacts}
+        selectedContact={to}
+        onSelect={contact => setTo(contact)}
+      />
+      <Chat contact={to} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+const contacts = [
+  { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
+  { id: 1, name: 'Alice', email: 'alice@mail.com' },
+  { id: 2, name: 'Bob', email: 'bob@mail.com' }
+];
